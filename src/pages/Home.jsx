a6858a6375cd08adc67c1ae79bceb34406a1176b
Abhinav Nath <abhinavnath@ymail.com>
2025-02-16 23:08:36 +0530

    Setup Routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "The Shawshank Redemption", year: 1994 },
     { id: 2, title: "The Godfather", year: 1972 },
-    { id: 3, title: "The Godfather: Part II", year: 1974 },
-    { id: 4, title: "The Dark Knight", year: 2008 },
+    { id: 3, title: "Harry Potter and the Chamber of Secrets", year: 2002 },
+    { id: 4, title: "Avengers: Endgame", year: 2019 },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(`Searching for ${searchQuery}`);
+    setSearchQuery("");
+  };
 
   return (
     <div className="home">
@@ -17,15 +24,20 @@ function Home() {
           type="text"
           placeholder="Search for movies"
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-button">
           Search
         </button>
       </form>
       <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+        {movies.map(
+          (movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery) && (
+              <MovieCard key={movie.id} movie={movie} />
+            )
+        )}
       </div>
     </div>
   );
